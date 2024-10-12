6851cbc9018b9638396d64e48565225d80b0b689
Mario <mario.bravo.82@gmail.com>
2024-10-12 19:13:16 -0300

    Finalizado, Creo!?

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
 import express from 'express';
+import productsRouter from './routes/products.js';
+import cartsRouter from './routes/carts.js';
 
 const Port = 8080;
 const app = express();
+
 app.use(express.json());
-app.use(express.urlencoded ({ extended: true}));
 
-app.get('/produts/', (req, res) => {
-    res.status(200).send({error: null, message: 'OK', data: 'test2' });
-});
-
-app.post('/', (req, rep) => {
-    rep.status(200).send({ error: null, message: 'ok', data: 'statuspost'})
-})
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
 app.listen(Port, () => {
-    console.log(' Server Activo')
+    console.log('Server Activo en localhost: ', Port)
 });
